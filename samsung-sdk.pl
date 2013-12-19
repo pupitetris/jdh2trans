22d20f8a54fc51472605bdb3821802858d77e2f4
Arturo Espinosa <arturo.espinosa@xamarin.com>
2013-12-19 13:10:03 -0600

    Easy comment-switching between parse/dumping and loading.
    First xml tests for commits, using Samsung.Android.Sdk namespace only.

--- samsung-sdk.pl
+++ samsung-sdk.pl
@@ -3,7 +3,7 @@
 # For testing purposes, this program can be invoked in this fashion 
 # to avoid module installation:
 
-# $ perl -I Xam-Binding-Trans/lib ./example.pl ~/SamsungSDK/source/external/Samsung_Mobile_SDK/Docs/API\ Reference
+# $ perl -I Xam-Binding-Trans/lib ./samsung-sdk.pl ~/Work/components/SamsungSDK/source
 
 use strict;
 use warnings FATAL => 'all';
@@ -12,10 +12,8 @@ use Data::Dumper;
 use Xam::Binding::Trans;
 
 # Command-line arguments.
-my $BASEDIR = $ARGV[0]; # Base directory for the HTML documentation.
-
-die 'BASEDIR not specified' if $BASEDIR eq '';
-
+my $BASEDIR = shift @ARGV; # Base directory for the HTML documentation.
+die 'BASEDIR not specified' if !$BASEDIR;
 
 # Configuration:
 
@@ -37,77 +35,71 @@ $Xam::Binding::Trans::PARAM_NAME_ENUM_EXCLUDE_RE = qr/^duration$/;
 # This is the default: non-int constants are ommited from CONSTS structure to simplify reports.
 $Xam::Binding::Trans::ONLY_PARSE_INT_CONSTANTS = 1;
 
+# Parse the documentation.
+sub parse {
+	my $basedir = shift;
 
-# Create parser instance:
-my $trans = Xam::Binding::Trans->new ();
-$trans->parse ($BASEDIR);
+	# Create parser instance:
+	my $trans = Xam::Binding::Trans->new ();
+	$trans->parse ($basedir);
 
-# Some classes are supposed to implement SsdkInterface:isFeatureEnabled(int) and 
-# the valid values provided by the class for this method are not explicitly documented.
-foreach my $key (keys %{$trans->{METHODS}}) {
-	next if $key !~ /isFeatureEnabled\(int\)$/;
+	# Some classes are supposed to implement SsdkInterface:isFeatureEnabled(int) and 
+	# the valid values provided by the class for this method are not explicitly documented.
+	foreach my $key (keys %{$trans->{METHODS}}) {
+		next if $key !~ /isFeatureEnabled\(int\)$/;
 
-	my $meth = $trans->{METHODS}->{$key};
-	my $param_name = uc ($meth->{PARAMS}->[0]->{NAME});
-	$param_name = 'FEATURE' if $param_name eq 'ID';
-	my $prefix = $meth->{CLASS}->{FULLNAME} . ".${param_name}_";
-	my $values = $trans->_collect_values_by_prefix ($prefix);
+		my $meth = $trans->{METHODS}->{$key};
+		my $param_name = uc ($meth->{PARAMS}->[0]->{NAME});
+		$param_name = 'FEATURE' if $param_name eq 'ID';
+		my $prefix = $meth->{CLASS}->{FULLNAME} . ".${param_name}_";
+		my $values = $trans->_collect_values_by_prefix ($prefix);
 
-	if (scalar keys %$values == 0) {
-		# Maybe nasty consts with unconventional names.
-		if ($key eq 'com.samsung.android.sdk.multiwindow.SMultiWindow.isFeatureEnabled(int)') {
-			$values = $trans->_collect_values_by_prefix ('com.samsung.android.sdk.multiwindow.SMultiWindow.MULTIWINDOW');
-			$param_name = 'type';
-		} elsif ($key eq 'com.samsung.android.sdk.pen.Spen.isFeatureEnabled(int)') {
-			$values = $trans->_collect_values_by_prefix ('com.samsung.android.sdk.pen.Spen.DEVICE_');
-			$param_name = 'type';
-		} else {
-			# OK, maybe it is not implemented.
-			next;
+		if (scalar keys %$values == 0) {
+			# Maybe nasty consts with unconventional names.
+			if ($key eq 'com.samsung.android.sdk.multiwindow.SMultiWindow.isFeatureEnabled(int)') {
+				$values = $trans->_collect_values_by_prefix ('com.samsung.android.sdk.multiwindow.SMultiWindow.MULTIWINDOW');
+				$param_name = 'type';
+			} elsif ($key eq 'com.samsung.android.sdk.pen.Spen.isFeatureEnabled(int)') {
+				$values = $trans->_collect_values_by_prefix ('com.samsung.android.sdk.pen.Spen.DEVICE_');
+				$param_name = 'type';
+			} else {
+				# OK, maybe it is not implemented.
+				next;
+			}
 		}
+
+		if (scalar keys %$values == 0) {
+			$DB::single = 1;
+			die;
+		}
+
+		my $new_enum = $trans->_create_enum_straight ($values, $param_name);
+		$trans->_method_set_param_type ($meth, 0, $new_enum);
 	}
 
-	if (scalar keys %$values == 0) {
-		$DB::single = 1;
-		die;
-	}
+	# Animation types
+	my $pkg = 'com.samsung.android.sdk.visualview.animation';
+	my $values = $trans->_collect_values_by_prefix ($pkg . '.SVAnimation.');
+	my $enum = $trans->_create_enum_straight ($values, 'TYPE');
+	$trans->_method_set_param_type ("$pkg.SVBasicAnimation.constructor(int,float[],float[])", 0, $enum);
+	$trans->_method_set_param_type ("$pkg.SVBasicAnimation.constructor(int,float,float)", 0, $enum);
+	$trans->_method_set_param_type ("$pkg.SVKeyFrameAnimation.constructor(int)", 0, $enum);
 
-	my $new_enum = $trans->_create_enum_straight ($values, $param_name);
-	$trans->_method_set_param_type ($meth, 0, $new_enum);
+	return $trans;
 }
 
-# Animation types
-my $pkg = 'com.samsung.android.sdk.visualview.animation';
-my $values = $trans->_collect_values_by_prefix ($pkg . '.SVAnimation.');
-my $enum = $trans->_create_enum_straight ($values, 'TYPE');
-$trans->_method_set_param_type ("$pkg.SVBasicAnimation.constructor(int,float[],float[])", 0, $enum);
-$trans->_method_set_param_type ("$pkg.SVBasicAnimation.constructor(int,float,float)", 0, $enum);
-$trans->_method_set_param_type ("$pkg.SVKeyFrameAnimation.constructor(int)", 0, $enum);
+my $dumpfile = 'dumps/state';
 
-$DB::single = 1;
+#my $trans = parse ($BASEDIR . '/external/Samsung_Mobile_SDK/Docs/API Reference'); $trans->dump ($dumpfile);
+my $trans = Xam::Binding::Trans::load ($dumpfile);
 
-$trans->dump ();
+my $dir;
+my $pkg;
 
-#$trans->printEnumFieldMapping (\*STDOUT, sort keys %{$trans->{PACKAGES}});
-#$trans->printEnumMethodMapping (\*STDOUT, qr/^com.samsung.android.sdk.visualview/);
-#$trans->printMetadata (\*STDOUT);
-
-$Data::Dumper::Indent = 1;
-$Data::Dumper::Sortkeys = 1;
-
-#$Data::Dumper::Varname = 'CLASSES';
-#print Dumper ($trans->{CLASSES});
-
-#$Data::Dumper::Varname = 'CONSTS';
-#print Dumper ($trans->{CONSTS});
-
-#$Data::Dumper::Varname = 'ENUMS';
-#print Dumper ($trans->{ENUMS});
-
-#$Data::Dumper::Varname = 'METHODS';
-#print Dumper ($trans->{METHODS});
-
-#$Data::Dumper::Varname = 'SELF';
-#print Dumper ($trans);
+$pkg = 'com.samsung.android.sdk';
+$dir = $BASEDIR . '/Samsung.Android.Sdk/Transforms';
+$trans->printEnumFieldMapping ($dir . '/EnumFields-2.xml', $pkg);
+$trans->printEnumMethodMapping ($dir . '/EnumMethods-2.xml', $pkg);
+$trans->printMetadata ($dir . '/Metadata-2.xml', $pkg);
 
 1;
